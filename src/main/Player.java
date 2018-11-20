844f50faef1ecf8dd879bc5e709592edc345b339
Demres <bialy1423@gmail.com>
2018-11-20 10:01:33 +0100

    getTerining

--- src/main/Player.java
+++ src/main/Player.java
@@ -7,7 +7,17 @@ public class Player {
 	private String name;
 	private String surname;
 	public List<Training> trainings = new ArrayList<Training>();
+	
 	public void addTraining(String n) {
 		trainings.add(new Training(n));
+	}
+	
+	public Training getTraining(String name) {
+		for(Training t: trainings) {
+			if(t.name == name) {
+				return t;
+			}
+		}
+		return null;
 	}
 }
