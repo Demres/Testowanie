b7385004d39931e8e248e2cedfa9aeb57274219e
test <test@test.com>
2018-11-20 09:37:39 +0100

    Training test

---
+++ src/test/TrainingTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import main.Player;
+
+public class TrainingTest {
+
+	@Test
+	public void test() {
+		Training t =new Training();
+		assertNotNull(t);
+	}
+
+}
